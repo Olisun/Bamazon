95ed1f2bb5de50e98cb201d045947efef779f9ac
Oliver Sun <olisun@mac.com>
2019-10-01 22:36:42 -0700

    added code for the transaction in js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,9 +29,9 @@ function welcomeToBamazon() {
       default: true
     }
     // Using a promise to show the invetory or shoo the user away if they don't want to spend. 
-  ]).then(function(userResponse) {
-    if (userResponse.viewInventory === true) {
-
+  ]).then(function(userResponseOne) {
+    if (userResponseOne.viewInventory === true) {
+      // If the user choses yes, then show the table created below.
       showInventory();
     } else {
       console.log('Okey-Doke. Come back when you want to buy something.')
@@ -58,5 +58,39 @@ function showInventory() {
     }
     // Calling on cli-table's method to console-log the table. 
     console.log(table.toString());
+    transaction();
   });
 };
+
+function transaction() {
+  inquirer.prompt([
+    // Using inquirer to as the user to select an item id and the number of items they want to buy. 
+    {
+      type: 'input',
+      name: 'itemId',
+      message: 'Please select the ITEM ID.'
+    },
+    {
+      type: 'input',
+      name: 'numberOfItems',
+      message: 'Please select the quantity you want to purchase.'
+    }
+    // Using a promise to query the database
+  ]).then(function(userRespsonseTwo) {
+    //
+    connection.query('SELECT * FROM products WHERE item_id=?', userRespsonseTwo.itemId, function(error, response) {
+      for (var i = 0; i < response.length; i++) {
+        if (userRespsonseTwo.numberOfItems <= response[i].stock_quantity) {
+          console.log(`We have your item(s) in stock. You selected the following:`)
+          console.log(`Item Selected: ${response[i].product_name}`);
+          console.log(`Department: ${response[i].department_name}`);
+          console.log(`Price: ${response[i].price}`);
+          console.log(`Number of Items: ${userRespsonseTwo.numberOfItems}`);
+          console.log(`Total Purchase Price: ` + parseInt(userRespsonseTwo.numberOfItems) * parseFloat(response[i].price));
+        } else {
+          console.log(`Sorry, we don't have enough in stock to fill your order.`)
+        }
+      }
+    });
+  });
+}
